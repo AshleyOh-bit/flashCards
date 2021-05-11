511858934aa38cce3549dd0250aa3121aaa63497
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-05-11 13:07:36 -0600

    Add countCards test

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -16,7 +16,7 @@ describe('Deck', function() {
     expect(deck).to.be.an.instanceof(Deck);
   })
 
-  it.skip('should take an array on instantiation', function() {
+  it.skip('should store a collection of cards', function() {
     const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
     const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
@@ -25,4 +25,15 @@ describe('Deck', function() {
     expect(deck.stack).to.deep.equal([card1, card2, card3]);
   })
 
+  it.skip('should be able to count the cards in its deck', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+
+    const deck = new Deck([card1, card2, card3]);
+
+    deck.countCards();
+
+    expect(deck.countCards).to.equal(3);
+  })
 }
