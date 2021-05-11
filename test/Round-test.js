759b8eeb257641b4049b38067642c1db75917dd8
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-05-11 15:49:47 -0600

    Add test to check deck stored in round

--- test/Round-test.js
+++ test/Round-test.js
@@ -15,12 +15,21 @@ describe('Round', function() {
     const round = new Round();
     expect(round).to.be.an.instanceof(Round);
   })
-//assert that currentCard is first card in deck.stack
-  //later in the test? after takeTurn?
 
 //returnCurrentCard() method
   // returns current card being played (turn.currentCard)
+  it('should store a deck of cards', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal',
+      ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?',
+      ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?',
+      ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
 
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    expect(round.deck).to.deep.equal(deck.stack);
+  })
 //takeTurn() method:
     // instantiates Turn w/ first index of deck.stack as currentCard argument
     // updates turns count,
@@ -30,6 +39,9 @@ describe('Round', function() {
     // gives feedback (turn.giveFeedback())
     // stores ids of incorrect guesses
       // create an empy array property on Round class?
+
+//assert that currentCard is first card in deck.stack
+  //later in the test? after takeTurn?
 
  // calculatePercentCorrect()
     // calculates and returns the percentage of correct guesses
