7087544968ff649e8421bb0bf74b6b9bca36fbde
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-05-11 13:36:45 -0600

    Complete Deck class setup

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -6,17 +6,17 @@ const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 
 describe('Deck', function() {
-  it.skip('should be a function', function() {
+  it('should be a function', function() {
     //const deck = new Deck();
     expect(Deck).to.be.a('function');
   })
 
-  it.skip('should be an instance of Turn', function() {
+  it('should be an instance of Deck', function() {
     const deck = new Deck();
     expect(deck).to.be.an.instanceof(Deck);
   })
 
-  it.skip('should store a collection of cards', function() {
+  it('should store a collection of cards', function() {
     const card1 = new Card(1, 'What is Robbie\'s favorite animal',
       ['sea otter', 'pug', 'capybara'], 'sea otter');
     const card2 = new Card(14, 'What organ is Khalid missing?',
@@ -28,7 +28,8 @@ describe('Deck', function() {
     expect(deck.stack).to.deep.equal([card1, card2, card3]);
   })
 
-  it.skip('should be able to count the cards in its deck', function() {
+  it('should be able to count the cards in its deck', function() {
+    //Should this be a forEach instead?
     const card1 = new Card(1, 'What is Robbie\'s favorite animal',
       ['sea otter', 'pug', 'capybara'], 'sea otter');
     const card2 = new Card(14, 'What organ is Khalid missing?',
