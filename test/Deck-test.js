5afcd7d8c99a771d337782205db0ba572336741e
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-05-11 13:01:05 -0600

    Add test to check for array on deck initialization

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -16,4 +16,13 @@ describe('Deck', function() {
     expect(deck).to.be.an.instanceof(Deck);
   })
 
+  it.skip('should take an array on instantiation', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+
+    const deck = new Deck([card1, card2, card3]);
+    expect(deck.stack).to.deep.equal([card1, card2, card3]);
+  })
+
 }
