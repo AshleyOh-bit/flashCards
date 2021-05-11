1d159c648e1aeda8df633907a8a81e92e6a5683f
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-05-11 12:56:35 -0600

    Add function/class check tests

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -6,6 +6,14 @@ const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 
 describe('Deck', function() {
+  it.skip('should be a function', function() {
+    const deck = new Deck();
+    expect(Deck).to.be.a('function');
+  })
 
+  it.skip('should be an instance of Turn', function() {
+    const deck = new Deck();
+    expect(deck).to.be.an.instanceof(Deck);
+  })
 
 }
