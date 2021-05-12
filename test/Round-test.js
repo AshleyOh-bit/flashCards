aed078c1cffe904154610c325ece3ea03390d960
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-05-11 19:37:05 -0600

    Refactor Round-test

--- test/Round-test.js
+++ test/Round-test.js
@@ -1,33 +1,32 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const Turn = require('../src/Turn');
+//const Turn = require('../src/Turn');
 const Card = require('../src/Card');
 const Deck = require('../src/Deck');
+const Round = require('../src/Round');
 
 describe('Round', function() {
   let card1, card2, card3, deck, round;
 
-    beforeEach(function() {
-      const card1 = new Card(1, 'What is Robbie\'s favorite animal',
-        ['sea otter', 'pug', 'capybara'], 'sea otter');
-      const card2 = new Card(14, 'What organ is Khalid missing?',
-        ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-      const card3 = new Card(12, 'What is Travis\'s middle name?',
-        ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+  beforeEach(function() {
+    card1 = new Card(1, 'What is Robbie\'s favorite animal',
+      ['sea otter', 'pug', 'capybara'], 'sea otter');
+    card2 = new Card(14, 'What organ is Khalid missing?',
+      ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    card3 = new Card(12, 'What is Travis\'s middle name?',
+      ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
 
-      const deck = new Deck([card1, card2, card3]);
-      const round = new Round(deck);
+    deck = new Deck([card1, card2, card3]);
+    round = new Round(deck);
   })
 
   it.skip('should be a function', function() {
-    //const deck = new Deck();
     expect(Round).to.be.a('function');
   })
 
-//Do I need to make it more explicit that I'm passing in an object as a parameter?
+  //Do I need to make it more explicit that it's an object as a parameter?
   it.skip('should be an instance of Deck', function() {
-    //const round = new Round();
     expect(round).to.be.an.instanceof(Round);
   })
 
@@ -35,70 +34,67 @@ describe('Round', function() {
     expect(round.deck).to.deep.equal(deck.stack);
   })
 
-//is this one really necessary?
-//Should we make a test to assert that turns is a prop?
+  //is this one really necessary?
+  //Should we make a test to assert that turns is a prop?
   it.skip('should be able to store incorrect guesses', function() {
     expect(round.incorrectGuesses).to.deep.equal([])
   })
 
-  //returnCurrentCard() method
-    // returns current card being played (turn.currentCard)
-    it.skip('should be able to return current card', function() {
-      deck.returnCurrentCard()
-      expect(round.returnCurrentCard()).to.deep.equal(deck.stack[0]);
-    })
+  //returnCurrentCard()
+  it.skip('should be able to return current card', function() {
+    deck.returnCurrentCard()
+    expect(round.returnCurrentCard()).to.deep.equal(deck.stack[0]);
+  })
 
-//takeTurn() method:
-    it.skip('should be able to update turn count', function() {
-      round.takeTurn('pug');
-      expect(round.turnCount).to.equal(1);
-    })
+  //takeTurn() method:
+  it.skip('should be able to update turn count', function() {
+    round.takeTurn('pug');
+    expect(round.turnCount).to.equal(1);
+  })
 
-    it.skip('should be able to track and store incorrect answers', function() {
-      round.takeTurn('sea otter');
-      expect(round.incorrectGuesses[0]).to.equal(1);
-    })
+  it.skip('should be able to store card ids of incorrect answers', function() {
+    round.takeTurn('sea otter');
+    expect(round.incorrectGuesses[0]).to.equal(1);
+  })
 
-    it.skip('should be able to give feedback', function() {
-      //const turn = new Turn('pug', card1);
-      round.takeTurn('capybara');
-      expect(round.takeTurn()).to.equal('correct!');
-    })
+  //Perhaps add a happy path to test here?
+  it.skip('should be able to evaluate guess and give feedback', function() {
+    round.takeTurn('pug');
+    expect(round.takeTurn()).to.equal('incorrect!');
+  })
 
-    it.skip('should be able to update current card', function() {
-      //const turn = new Turn('pug', card1);
-      round.takeTurn('capybara');
-      //instantiates new turn
-      expect(round.currentCard).to.equal(this.deck[1]);
-    })
+  it.skip('should be able to update current card', function() {
+    round.takeTurn('capybara');
+    expect(round.currentCard).to.equal(this.deck[1]);
+  })
 
-    //how TF do i do this lol
-    // it.skip('should be able to instantiate a new Turn', function() {
-    //   //const turn = new Turn('pug', card1);
-    //   round.takeTurn('capybara');
-    //   //instantiates new turn
-    //   expect().to.equal();
-    // })
+  //how TF do i do this lol
+  // it.skip('should be able to instantiate a new Turn', function() {
+  //   //const turn = new Turn('pug', card1);
+  //   round.takeTurn('capybara');
+  //   //instantiates new turn
+  //   expect().to.equal();
+  // })
 
- // calculatePercentCorrect()
-    it.skip('should be able to calculate the percentage of correct guesses', function() {
-      round.takeTurn('pug');
-      round.takeTurn('capybara');
+  // calculatePercentCorrect()
+  it.skip('should calculate the percentage of correct guesses', function() {
+    round.takeTurn('pug');
+    round.takeTurn('capybara');
 
-      round.calculatePercentCorrect()
+    round.calculatePercentCorrect()
 
-      expect(round.calculatePercentCorrect()).to.deep.equal(50);
-    })
+    expect(round.calculatePercentCorrect()).to.deep.equal(50);
+  })
 
- // endRound()
-    it.skip('should be able to announce end of round', function() {
-      round.takeTurn('pug');
-      round.takeTurn('capybara');
+  // endRound()
+  it.skip('should be able to announce end of round', function() {
+    round.takeTurn('pug');
+    round.takeTurn('capybara');
 
-      round.calculatePercentCorrect()
-      round.endRound()
+    round.calculatePercentCorrect()
+    round.endRound()
 
-      expect(round.endRound()).to.deep.equal(' ‘** Round over! ** You answered 50% of the questions correctly!’');
-    })
+    expect(round.endRound()).to.deep.equal('** Round over! ** You answered 50% of the questions correctly!');
+  })
 
-}
+})
