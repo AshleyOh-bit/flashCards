7a7595e3a9833c64be7869773053bfaedaeeb92f
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-05-13 17:22:59 -0600

    Lint Game-test

--- test/Game-test.js
+++ test/Game-test.js
@@ -1,25 +1,15 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const Turn = require('../src/Turn');
 const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 const Round = require('../src/Round');
 const Game = require('../src/Game');
 
 describe('Game', function() {
-  let card1, card2, card3, deck, round, game, data;
+  let game;
 
   beforeEach(function() {
-    card1 = new Card(1, 'What is Robbie\'s favorite animal',
-      ['sea otter', 'pug', 'capybara'], 'sea otter');
-    card2 = new Card(14, 'What organ is Khalid missing?',
-      ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    card3 = new Card(12, 'What is Travis\'s middle name?',
-      ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-
-    deck = new Deck([card1, card2, card3]);
-    round = new Round(deck);
     game = new Game();
   })
 
@@ -44,6 +34,8 @@ describe('Game', function() {
   it('should store an array of Cards', function() {
     game.start();
     expect(game.currentRound.deck.stack[0]).to.an.instanceof(Card);
+    expect(game.currentRound.deck.stack[1]).to.an.instanceof(Card);
+    expect(game.currentRound.deck.stack[2]).to.an.instanceof(Card);
   })
 
 })
