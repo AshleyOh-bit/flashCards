c357e500222f1c99c88386f1d2ac403555f123b7
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-05-12 19:33:43 -0600

    Add notes to Game class

--- src/Game.js
+++ src/Game.js
@@ -5,6 +5,17 @@ const util = require('./util');
 class Game {
   constructor() {}
 
+  start() {
+    //creates cards
+    //creates a new Round using Deck
+
+    //invokes printMessage to display message on CLI
+    //invokes printQuestion to kick off helper functions
+    data.map((element, index) => {
+      let card[index] = new Card(element.id, element.question, element.answers, element.correctAnswer)
+    })
+  }
+
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
 -----------------------------------------------------------------------`)
