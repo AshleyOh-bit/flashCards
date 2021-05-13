adf201e6d182d9f417ce6b631c2e2496e7ec74c2
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-05-13 16:46:10 -0600

    Refactor Deck-test

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -1,46 +1,37 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-//const Turn = require('../src/Turn');
 const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 
 describe('Deck', function() {
+  let card1, card2, card3, deck;
+
+  beforeEach(function() {
+    card1 = new Card(1, 'What is Robbie\'s favorite animal',
+      ['sea otter', 'pug', 'capybara'], 'sea otter');
+    card2 = new Card(14, 'What organ is Khalid missing?',
+      ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    card3 = new Card(12, 'What is Travis\'s middle name?',
+      ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+
+    deck = new Deck([card1, card2, card3]);
+  })
+
   it('should be a function', function() {
-    //const deck = new Deck();
     expect(Deck).to.be.a('function');
   })
 
   it('should be an instance of Deck', function() {
-    const deck = new Deck();
     expect(deck).to.be.an.instanceof(Deck);
   })
 
   it('should store a collection of cards', function() {
-    const card1 = new Card(1, 'What is Robbie\'s favorite animal',
-      ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card(14, 'What organ is Khalid missing?',
-      ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(12, 'What is Travis\'s middle name?',
-      ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-
-    const deck = new Deck([card1, card2, card3]);
     expect(deck.stack).to.deep.equal([card1, card2, card3]);
   })
 
   it('should be able to count the cards in its deck', function() {
-    //Should this be a forEach instead?
-    const card1 = new Card(1, 'What is Robbie\'s favorite animal',
-      ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card(14, 'What organ is Khalid missing?',
-      ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(12, 'What is Travis\'s middle name?',
-      ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-
-    const deck = new Deck([card1, card2, card3]);
-
     deck.countCards();
-
     expect(deck.countCards()).to.equal(3);
   })
 
