3692df009876d9783ce7c58410dd1c3b148cbf02
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-05-12 19:35:23 -0600

    Add function and instance tests

--- test/Game-test.js
+++ test/Game-test.js
@@ -8,9 +8,15 @@ const Round = require('../src/Round');
 const Game = require('../src/Game');
 
 describe('Round', function() {
-  let card1, card2, card3, deck, round;
+  let card1, card2, card3, deck, round, game, data;
 
   beforeEach(function() {
+    // data = [{"id": 1, "question": 'What is Robbie\'s favorite animal',
+    //   "answers": ['sea otter', 'pug', 'capybara'], "correctAnswer": 'sea otter'},
+    //   {"id": 14, "question": 'What organ is Khalid missing?',
+    //     "answers": ['spleen', 'appendix', 'gallbladder'], "correctAnswer": 'gallbladder'}, {"id": 12, "question": 'What is Travis\'s middle name?',
+    //       "answers": ['Lex', 'William', 'Fitzgerald'], "correctAnswer": 'Fitzgerald'}]
+
     card1 = new Card(1, 'What is Robbie\'s favorite animal',
       ['sea otter', 'pug', 'capybara'], 'sea otter');
     card2 = new Card(14, 'What organ is Khalid missing?',
@@ -18,8 +24,22 @@ describe('Round', function() {
     card3 = new Card(12, 'What is Travis\'s middle name?',
       ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
 
-    deck = new Deck([card1, card2, card3]);
-    round = new Round(deck);
+    //deck = new Deck([card1, card2, card3]);
+    //round = new Round(deck);
+    game = new Game();
   })
+
+  it.skip('should be a function', function() {
+    expect(Game).to.be.a('function');
+  })
+
+  it.skip('should be an instance of Game', function() {
+    expect(game).to.be.an.instanceof(Game);
+  })
+
+//start method creates current Round
+//should keep track of the curentRound
+
+
 
 })
